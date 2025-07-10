7cd530328b6b2528de4304c02c1a918961751121
namanp13 <naman.pujani@gmail.com>
2025-07-09 21:25:59 -0400

    Final commit

--- app.R
+++ app.R
@@ -1,89 +1,111 @@
 library(shiny)
-library(httr)
-library(jsonlite)
-library(tibble)
-library(purrr)
+library(ggplot2)
 library(dplyr)
+library(DT)
+library(purrr)
+library(jsonlite)
+library(httr)
 
-source("functions.R")
+#Couple of notes: I used some different syntax: .data[[input$var]]. The reason for that is because I realized I can't write aes(x = input$x_var) because that would make it think that input$x_var is the column itself. All .data[[input$x_var]] does is it tells ggplot to look at the column of the dataframe. I also read somewhere that this is better for more interactive apps. I also used req(input$x_var) because I was getting an error where input$x_var was NULL. I looked it up online and found that req() was the solution to it (I think at shiny.posit.co). 
 
-# Prepare team choices
+api_key <- "1f08e5ca-ec1f-4a50-805b-203ad159024c"
+
+get_player <- function(name, api_key) {
+  url <- paste0("https://api.balldontlie.io/v1/players?search=", name)
+  resp <- GET(url, add_headers(Authorization = api_key))
+  content(resp, as = "text", encoding = "UTF-8") |>
+    fromJSON() |>
+    pluck("data") |>
+    tibble::as_tibble()
+}
+
+get_teams <- function(api_key) {
+  url <- "https://api.balldontlie.io/v1/teams"
+  resp <- GET(url, add_headers(Authorization = api_key))
+  content(resp, as = "text", encoding = "UTF-8") |>
+    fromJSON() |>
+    pluck("data") |>
+    tibble::as_tibble()
+}
+
 teams_data <- get_teams(api_key)
-teams_choices <- reactive({
-  data <- get_teams(api_key)
-  setNames(data$id, data$full_name)
-})
 
-ui <- fluidPage(
-  titlePanel("NBA Stats Explorer"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("query_type", "Select Query Type", choices = c(
-        "Search Players by Name",
-        "List All Teams",
-        "Filter Teams by Conference",
-        "Games for Team on Date",
-        "All Games on Date",
-        "Games for Team over Date Range"
-      )),
-      uiOutput("query_inputs")
-    ),
-    mainPanel(
-      tableOutput("api_data"),
-      downloadButton("download_data", "Download CSV")
-    )
-  )
-)
+ui <- navbarPage(
+  "NBA Stats Explorer",
+  
+  tabPanel("About",
+           fluidPage(h2("About this App"), p("This app allows you to explore NBA team and player data. There's different information about players regarding their teams, names, cities, height, weight, and so on. It comes from the ", a("balldontlie API", href = "https://www.balldontlie.io/"), "."), p("Tabs:"), tags$ul(tags$li(strong("Data Download:"), " Allows you to query players or teams. It also allows you to subset data or download it."),tags$li(strong("Data Exploration:"), " Allows you to summarize and visualize NBA data with different plots and tables.")),img(src = "nba_logo.png", height = "300px"))
+  ),
+  
+  tabPanel("Data Download", sidebarLayout(sidebarPanel(textInput("player_name", "Search Player Name (leave blank for teams):"),actionButton("query", "Query Data"),hr(), checkboxGroupInput("columns", "Include columns:", choices = NULL),
+downloadButton("download", "Download CSV")),
+  
+  mainPanel(DTOutput("data_table")))),
+  
+  tabPanel("Data Exploration",sidebarLayout(sidebarPanel(selectInput("dataset", "Select dataset:", choices = c("Teams", "Players")),uiOutput("xvar_ui"), uiOutput("facet_ui"), selectInput("plot_type", "Plot Type:", choices = c("Bar", "Heatmap"))),
+  
+  mainPanel(tableOutput("summary_table"),plotOutput("explore_plot")))))
 
 server <- function(input, output, session) {
-  output$query_inputs <- renderUI({
-    switch(input$query_type,
-           "Search Players by Name" = textInput("player_name", "Enter Player Name", ""),
-           "List All Teams" = NULL,
-           "Filter Teams by Conference" = selectInput("conference", "Select Conference", c("East", "West")),
-           "Games for Team on Date" = tagList(
-             selectInput("team_id", "Select Team", choices = teams_choices()),
-             dateInput("game_date", "Select Date")
-           ),
-           "All Games on Date" = dateInput("game_date_all", "Select Date"),
-           "Games for Team over Date Range" = tagList(
-             selectInput("team_id_range", "Select Team", choices = teams_choices),
-             dateRangeInput("game_date_range", "Select Date Range")
-           )
-    )
+  
+  queried_data <- reactiveVal(teams_data)
+  
+  observeEvent(input$query, {
+    if (input$player_name != "") {
+      queried_data(get_player(input$player_name, api_key))
+    } else {
+      queried_data(teams_data)
+    }
+    
+    updateCheckboxGroupInput(session, "columns", choices = names(queried_data()),selected = names(queried_data()))})
+  
+  output$data_table <- renderDT({
+    req(queried_data())
+    queried_data()[, input$columns, drop = FALSE]
   })
   
-  queried_data <- reactive({
-    req(input$query_type)
-    switch(input$query_type,
-           "Search Players by Name" = get_player(input$player_name, api_key),
-           "List All Teams" = get_teams(api_key),
-           "Filter Teams by Conference" = {
-             teams <- get_teams(api_key)
-             teams %>% filter(conference == input$conference)
-           },
-           "Games for Team on Date" = get_games(input$team_id, input$game_date, api_key),
-           "All Games on Date" = get_games(NULL, input$game_date_all, api_key),
-           "Games for Team over Date Range" = {
-             dates <- input$game_date_range
-             get_games(input$team_id_range, dates, api_key)
-           }
-    )
-  })
-  
-  output$api_data <- renderTable({
-    queried_data()
-  })
-  
-  output$download_data <- downloadHandler(
-    filename = function() {
-      paste0("nba_data_", Sys.Date(), ".csv")
-    },
-    content = function(file) {
-      write.csv(queried_data(), file, row.names = FALSE)
+  output$download <- downloadHandler(
+    filename = function() { "nba_data.csv" }, content = function(file) {
+      write.csv(queried_data()[, input$columns, drop = FALSE], file, row.names = FALSE)
     }
   )
-}
+  
+  output$xvar_ui <- renderUI({
+    req(input$dataset)
+    choices <- if (input$dataset == "Teams") names(teams_data) else names(queried_data())
+    selectInput("xvar", "X Variable:", choices = choices)
+  })
+  
+  output$facet_ui <- renderUI({
+    req(input$dataset)
+    choices <- if (input$dataset == "Teams") names(teams_data) else names(queried_data())
+    selectInput("facet", "Facet Variable:", choices = c("None", choices))
+  })
+  
+  output$summary_table <- renderTable({
+    data <- if (input$dataset == "Teams") teams_data else queried_data()
+    req(input$xvar)
+    data %>% count(.data[[input$xvar]])
+  })
+  
+  output$explore_plot <- renderPlot({
+    data <- if (input$dataset == "Teams") teams_data else queried_data()
+    req(input$xvar)
+    
+    if (input$plot_type == "Bar") {
+      p <- ggplot(data, aes(x = .data[[input$xvar]])) +
+        geom_bar(fill = "blue") +
+        labs(title = paste("Bar Plot of", input$xvar), x = input$xvar, y = "Count")
+    } else {
+      heatmap_data <- data %>%
+        count(.data[[input$xvar]], .data[[input$facet]])
+      p <- ggplot(heatmap_data, aes(x = .data[[input$xvar]], y = .data[[input$facet]], fill = n)) +
+        geom_tile() +
+        labs(title = paste("Heatmap of", input$xvar, "vs", input$facet), x = input$xvar, y = input$facet, fill = "Count")}
+    
+    if (input$facet != "None" && input$plot_type == "Bar") {
+      p <- p + facet_wrap(as.formula(paste("~", input$facet)))}
+    p})}
 
 shinyApp(ui, server)
 
--- functions.R
+++ functions.R
@@ -1,5 +1,3 @@
-api_key <- "1f08e5ca-ec1f-4a50-805b-203ad159024c"
-
 get_player <- function(name, api_key) {
   url <- paste0("https://api.balldontlie.io/v1/players?search=", name)
   response <- httr::GET(url, httr::add_headers(Authorization = api_key))
@@ -11,21 +9,12 @@ get_player <- function(name, api_key) {
 }
 
 
+
 get_teams <- function(api_key) {
   url2 <- "https://api.balldontlie.io/v1/teams"
   response2 <- httr::GET(url2, httr::add_headers(Authorization = api_key))
   
   httr::content(response2, as = "text", encoding = "UTF-8") |>
-    jsonlite::fromJSON() |>
-    purrr::pluck("data") |>
-    tibble::as_tibble()
-}
-
-get_games <- function(id, date, api_key) {
-  url3 <- paste0("https://api.balldontlie.io/v1/games?team_ids[]=", id, "&dates[]=", date)
-  response3 <- httr::GET(url3, httr::add_headers(Authorization = api_key))
-  
-  httr::content(response3, as = "text", encoding = "UTF-8") |>
     jsonlite::fromJSON() |>
     purrr::pluck("data") |>
     tibble::as_tibble()
